9df8a275eaa4de3ef38514591c73998e08653db9
Gourav Gupta <sorenpinky96@gmail.com>
2025-12-13 17:01:33 +0000

    Django Template using loop

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def home_page(request):
     data = {
         "title": "Home Page",
+        "bdata": "Welcome to razorfish",
+        "clist": ["PHP", "Java", "Django"],
+        "student_details": [
+            {"name": "gaurav gupta", "phone": 983248723},
+            {"name": "testing kumar", "phone": 23612323}
+        ]
     }
     return render(request, "index.html", data)
 
